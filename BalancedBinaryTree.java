31a03da729b46c05b64a935bab49f897cf25a0e5
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-07 11:05:55 +0530

    Update BalancedBinaryTree.java

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -1,1 +1,38 @@
 /*https://leetcode.com/problems/balanced-binary-tree/*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Pair{
+     boolean isB=true;
+     int ht=-1;
+ }
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+       return  isBal(root).isB;
+    }
+    public Pair isBal(TreeNode root){
+        if(root==null){
+            return new Pair();
+        }
+       Pair x= isBal(root.left);
+       Pair y= isBal(root.right);
+       Pair obj=new Pair();
+       obj.ht=Math.max(x.ht,y.ht)+1;
+      int bf=Math.abs(x.ht-y.ht);
+     obj.isB= x.isB && y.isB && bf<=1;
+     return obj;
+    }
+}
